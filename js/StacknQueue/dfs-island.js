58d9fa6a5ae5c43a17fbb63fd958972464647a56
SOUVIGHOSH <119578073+SOUVIGHOSH@users.noreply.github.com>
2023-05-31 14:01:46 +0530

    Update dfs-island.js

--- js/StacknQueue/dfs-island.js
+++ js/StacknQueue/dfs-island.js
@@ -8,18 +8,31 @@ const numIslands = function(grid) {
   const visited = new Set();
   let count = 0;
 
-  const dfs = function(row, col) {
-    if (row < 0 || row >= rows || col < 0 || col >= cols || grid[row][col] === '0' || visited.has(`${row}-${col}`)) {
-      return;
-    }
+  const stack = [];
 
+  const isValid = function(row, col) {
+    return row >= 0 && row < rows && col >= 0 && col < cols && grid[row][col] === '1' && !visited.has(`${row}-${col}`);
+  };
+
+  const dfs = function(row, col) {
+    const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+
+    stack.push([row, col]);
     visited.add(`${row}-${col}`);
 
-    // Explore neighbors
-    dfs(row - 1, col); // Up
-    dfs(row + 1, col); // Down
-    dfs(row, col - 1); // Left
-    dfs(row, col + 1); // Right
+    while (stack.length > 0) {
+      const [currRow, currCol] = stack.pop();
+
+      for (const [dx, dy] of directions) {
+        const newRow = currRow + dx;
+        const newCol = currCol + dy;
+
+        if (isValid(newRow, newCol)) {
+          stack.push([newRow, newCol]);
+          visited.add(`${newRow}-${newCol}`);
+        }
+      }
+    }
   };
 
   for (let i = 0; i < rows; i++) {
@@ -43,9 +56,8 @@ const grid = [
 ];
 
 const islandCount = numIslands(grid);
+console.log(islandCount); // Output: 1
 
 /*
-This approach effectively counts the number of islands in the grid by treating each land cell as the starting point of a DFS traversal. By exploring all adjacent land cells connected to each starting point, we ensure that we cover all cells belonging to the same island.
-
-The time complexity of this solution is O(m * n), where m is the number of rows and n is the number of columns in the grid. We visit each cell once, and the DFS from each cell explores the connected cells.
+The time complexity and space complexity of this solution remain O(m * n), where m is the number of rows and n is the number of columns in the grid.
 */
