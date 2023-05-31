d9dfa16002580894a4f01dcd1167be4d45061818
SOUVIGHOSH <119578073+SOUVIGHOSH@users.noreply.github.com>
2023-05-31 13:07:14 +0530

    Create stack.js

---
+++ js/StacknQueue/stack.js
@@ -0,0 +1,31 @@
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(item) {
+    this.stack.push(item);
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.stack.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.stack[this.stack.length - 1];
+  }
+
+  isEmpty() {
+    return this.stack.length === 0;
+  }
+
+  size() {
+    return this.stack.length;
+  }
+}
